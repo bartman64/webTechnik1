2402d5d0034bea100be66f033c0d94ed990e807a
Sebastian Horvath <se.bi1994@hotmail.de>
2017-11-18 17:10:21 +0100

    implemented table sorting integrated login in the navigationbar

--- FirstProject/res/js/login.js
+++ FirstProject/res/js/login.js
@@ -1,18 +1,25 @@
 function saveUsername() {
     var username = document.getElementById('userInput').value;
-    var re = new RegExp("^([a-zA-Z])$");
-    if (re.test(username)) {
-        localStorage.setItem("text", username);
-    } else {
-        console.log("no name");
+    var message = "Hello " + username;
+    localStorage.setItem("text", username);
+    var re = new RegExp("^[a-zA-Z]+$");
+    if (!re.test(username)) {
+        alert("Input not valid: Only letters allowed");
+        return false;
     }
-
+    document.getElementById("error_message").innerHTML = message;
 }
 
 window.onload = function () {
-    var username = localStorage.getItem("text");
-    localStorage.clear();
     if (document.getElementById("test") !== null) {
-        document.getElementById("test").innerHTML = "Hallo " + username;
+        var username = localStorage.getItem("text");
+        var message;
+        localStorage.clear();
+        if (username !== null) {
+            message = "Hallo " + username;
+        } else {
+            message = "Not logged in";
+        }
+        document.getElementById("test").value = message;
     }
 }
