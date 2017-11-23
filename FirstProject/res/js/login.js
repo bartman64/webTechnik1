aa311fd11bdac9afa1b3d28169714db918fff7f6
Sebastian Horvath <se.bi1994@hotmail.de>
2017-11-23 14:46:07 +0100

    did some stuff

--- FirstProject/res/js/login.js
+++ FirstProject/res/js/login.js
@@ -1,25 +1,31 @@
 function saveUsername() {
     var username = document.getElementById('userInput').value;
-    var message = "Hello " + username;
-    localStorage.setItem("text", username);
+
     var re = new RegExp("^[a-zA-Z]+$");
     if (!re.test(username)) {
-        alert("Input not valid: Only letters allowed");
+        //   alert("Input not valid: Only letters allowed");
+        document.getElementById("error_message").innerHTML = "Input not valid: Only letters allowed";
+
         return false;
+    } else {
+        localStorage.setItem("text", username);
     }
-    document.getElementById("error_message").innerHTML = message;
 }
 
 window.onload = function () {
-    if (document.getElementById("test") !== null) {
-        var username = localStorage.getItem("text");
-        var message;
-        localStorage.clear();
-        if (username !== null) {
-            message = "Hallo " + username;
-        } else {
-            message = "Not logged in";
-        }
-        document.getElementById("test").value = message;
+
+    var username = localStorage.getItem("text");
+    console.log(username);
+    var message = "Not logged in";
+    if (username !== null) {
+        message = "Hallo " + username;
     }
+
+    if (document.getElementById("test") !== null) {
+        document.getElementById("test").innerHTML = message;
+    }
+}
+
+function logout() {
+    localStorage.clear();
 }
