38989bcd09cba2f25b32d53d9149a030d100dbef
Sebastian Horvath <se.bi1994@hotmail.de>
2017-12-07 11:51:48 +0100

    did some stuff

--- FirstProject/res/js/login.js
+++ FirstProject/res/js/login.js
@@ -1,31 +1,53 @@
 function saveUsername() {
+    //Getting the username from the input field
     var username = document.getElementById('userInput').value;
 
+    //Regex which test if the string only contains letters
     var re = new RegExp("^[a-zA-Z]+$");
     if (!re.test(username)) {
-        //   alert("Input not valid: Only letters allowed");
+        //Displaying an error message
         document.getElementById("error_message").innerHTML = "Input not valid: Only letters allowed";
-
         return false;
     } else {
+        //Save the username in the localStorage
         localStorage.setItem("text", username);
+        window.location.reload();
     }
+    document.getElementById('userInput').value = "";
 }
 
 window.onload = function () {
-
+    //Get the username from the localStorage
     var username = localStorage.getItem("text");
-    console.log(username);
+
     var message = "Not logged in";
+
     if (username !== null) {
         message = "Hallo " + username;
     }
 
-    if (document.getElementById("test") !== null) {
-        document.getElementById("test").innerHTML = message;
+    for (var i = 0; i < 7; i++) {
+        if (document.getElementById("test" + i) !== null) {
+            if (i === 0) {
+                document.getElementById("test" + i).innerHTML = message;
+            } else {
+                document.getElementById("test" + i).innerHTML = username;
+            }
+        }
+    }
+}
+
+
+function swap(id) {
+    if (document.getElementById(id).style.display === "none") {
+        document.getElementById(id).style.display = "block";
+    }
+    else {
+        document.getElementById(id).style.display = "none";
     }
 }
 
 function logout() {
     localStorage.clear();
+    window.location.reload();
 }
